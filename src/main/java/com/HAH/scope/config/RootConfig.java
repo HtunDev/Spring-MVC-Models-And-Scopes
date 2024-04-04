7f3fcef1afc19eae46cbcddc5dc4bc35a1c475c7
hhtun137 <htunaunghlaing199@gmail.com>
2024-04-04 09:30:21 +0630

    web xml with javaBase Config

---
+++ src/main/java/com/HAH/scope/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.HAH.scope.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.HAH.scope.model")
+public class RootConfig {
+
+}
